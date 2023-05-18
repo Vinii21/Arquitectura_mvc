159e537124d88c58ffd8d1f796033df63749cb34
Vinicio Chavarria <vichavarria21@gmaail.com>
2023-05-18 12:41:52 -0600

    Actualizacion

--- src/utils/database.js
+++ src/utils/database.js
@@ -3,9 +3,9 @@ require("dotenv").config()
 
 const db = new Sequelize({
     host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
     port: process.env.DB_PORT,
     username: process.env.DB_USERNAME,
-    database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     dialect: "postgres",
     dialectOptions: { ssl: {require: true, rejectUnauthorized: false} }

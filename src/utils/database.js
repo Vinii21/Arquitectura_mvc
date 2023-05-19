277c9d45e92ad475de6990e03a15a0c5a897b946
Vinicio Chavarria <vichavarria21@gmaail.com>
2023-05-19 13:21:53 -0600

    SSL

--- src/utils/database.js
+++ src/utils/database.js
@@ -8,7 +8,7 @@ const db = new Sequelize({
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     dialect: "postgres",
-   /*  dialectOptions: { ssl: {require: true, rejectUnauthorized: false} } */
+    dialectOptions: { ssl: {require: true, rejectUnauthorized: false} }
 })
 
 module.exports = db;
